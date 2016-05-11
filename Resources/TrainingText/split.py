f1e70bc84d33b2288ac17fc5ad8759caf31f736a
Peter Rudenko <peter.rudenko@gmail.com>
2016-05-11 10:15:03 +0400

    Fixed the text splitting script.
    Former-commit-id: 1acc537b3a8196ed7206aac8845c72649ff4a262 [formerly 7c386763430bea0f7bc7382c5c0d40af34e40987]
    Former-commit-id: 4a843c75b7ff3930a968d016702f5c8ce5e14515

--- Resources/TrainingText/split.py
+++ Resources/TrainingText/split.py
@@ -13,12 +13,12 @@ batchCounter = int(sys.argv[3])
 
 with open(sys.argv[1], 'r') as inputFile:
     for line in inputFile:
-        strippedLine = line.strip()
+        strippedLine = line.decode('utf-8').strip().lower()
         if strippedLine != '\n' and strippedLine != '\r' and strippedLine != '\r\n' and strippedLine != '':
-            lineNoBreaks = strippedLine.replace('\n', '').replace('\r', '')
+            lineNoBreaks = strippedLine.replace('\n', '').replace('\r', '').encode('utf-8')
             textBatch = textBatch + lineNoBreaks + '\n'
         else:
-            outputFileName = outputFileNameBase + str(batchCounter) + '.txt'
+            outputFileName = outputFileNameBase + str(batchCounter).zfill(4) + '.txt'
             with open(outputFileName, "wb") as outputFile:
                 outputFile.write(textBatch)
             textBatch = ''
