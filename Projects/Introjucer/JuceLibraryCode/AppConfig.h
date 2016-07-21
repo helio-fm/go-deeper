714f02a2e616e0d5f07c1c74b80cd63116d9f7dc
Peter Rudenko <peter.rudenko@gmail.com>
2016-07-21 15:07:24 +0400

    Got rid of GUI and audio deps and re-saved the projects.

--- Projects/Introjucer/JuceLibraryCode/AppConfig.h
+++ Projects/Introjucer/JuceLibraryCode/AppConfig.h
@@ -22,16 +22,10 @@
 // [END_USER_CODE_SECTION]
 
 //==============================================================================
-#define JUCE_MODULE_AVAILABLE_juce_audio_basics         1
-#define JUCE_MODULE_AVAILABLE_juce_audio_devices        1
-#define JUCE_MODULE_AVAILABLE_juce_audio_formats        1
 #define JUCE_MODULE_AVAILABLE_juce_core                 1
 #define JUCE_MODULE_AVAILABLE_juce_cryptography         1
 #define JUCE_MODULE_AVAILABLE_juce_data_structures      1
 #define JUCE_MODULE_AVAILABLE_juce_events               1
-#define JUCE_MODULE_AVAILABLE_juce_graphics             1
-#define JUCE_MODULE_AVAILABLE_juce_gui_basics           1
-#define JUCE_MODULE_AVAILABLE_juce_gui_extra            1
 
 //==============================================================================
 #ifndef    JUCE_STANDALONE_APPLICATION
@@ -43,68 +37,6 @@
 #endif
 
 #define JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED 1
-
-//==============================================================================
-// juce_audio_devices flags:
-
-#ifndef    JUCE_ASIO
- #define   JUCE_ASIO 0
-#endif
-
-#ifndef    JUCE_WASAPI
- #define   JUCE_WASAPI 0
-#endif
-
-#ifndef    JUCE_WASAPI_EXCLUSIVE
- #define   JUCE_WASAPI_EXCLUSIVE 0
-#endif
-
-#ifndef    JUCE_DIRECTSOUND
- #define   JUCE_DIRECTSOUND 0
-#endif
-
-#ifndef    JUCE_ALSA
- #define   JUCE_ALSA 0
-#endif
-
-#ifndef    JUCE_JACK
- #define   JUCE_JACK 0
-#endif
-
-#ifndef    JUCE_USE_ANDROID_OPENSLES
- #define   JUCE_USE_ANDROID_OPENSLES 0
-#endif
-
-#ifndef    JUCE_USE_CDREADER
- #define   JUCE_USE_CDREADER 0
-#endif
-
-#ifndef    JUCE_USE_CDBURNER
- #define   JUCE_USE_CDBURNER 0
-#endif
-
-//==============================================================================
-// juce_audio_formats flags:
-
-#ifndef    JUCE_USE_FLAC
- #define   JUCE_USE_FLAC 0
-#endif
-
-#ifndef    JUCE_USE_OGGVORBIS
- #define   JUCE_USE_OGGVORBIS 0
-#endif
-
-#ifndef    JUCE_USE_MP3AUDIOFORMAT
- #define   JUCE_USE_MP3AUDIOFORMAT 0
-#endif
-
-#ifndef    JUCE_USE_LAME_AUDIO_FORMAT
- #define   JUCE_USE_LAME_AUDIO_FORMAT 0
-#endif
-
-#ifndef    JUCE_USE_WINDOWS_MEDIA_FORMAT
- #define   JUCE_USE_WINDOWS_MEDIA_FORMAT 0
-#endif
 
 //==============================================================================
 // juce_core flags:
@@ -131,47 +63,6 @@
 
 #ifndef    JUCE_USE_CURL
  //#define JUCE_USE_CURL
-#endif
-
-//==============================================================================
-// juce_graphics flags:
-
-#ifndef    JUCE_USE_COREIMAGE_LOADER
- #define   JUCE_USE_COREIMAGE_LOADER 0
-#endif
-
-#ifndef    JUCE_USE_DIRECTWRITE
- #define   JUCE_USE_DIRECTWRITE 0
-#endif
-
-//==============================================================================
-// juce_gui_basics flags:
-
-#ifndef    JUCE_ENABLE_REPAINT_DEBUGGING
- #define   JUCE_ENABLE_REPAINT_DEBUGGING 0
-#endif
-
-#ifndef    JUCE_USE_XSHM
- #define   JUCE_USE_XSHM 0
-#endif
-
-#ifndef    JUCE_USE_XRENDER
- #define   JUCE_USE_XRENDER 0
-#endif
-
-#ifndef    JUCE_USE_XCURSOR
- #define   JUCE_USE_XCURSOR 0
-#endif
-
-//==============================================================================
-// juce_gui_extra flags:
-
-#ifndef    JUCE_WEB_BROWSER
- #define   JUCE_WEB_BROWSER 0
-#endif
-
-#ifndef    JUCE_ENABLE_LIVE_CONSTANT_EDITOR
- #define   JUCE_ENABLE_LIVE_CONSTANT_EDITOR 0
 #endif
 
 
